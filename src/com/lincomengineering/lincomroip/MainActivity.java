b2a8e190514562db6f9dc232018fa1b698fe7e83
AlexVangelov <email@data.bg>
2013-05-15 14:06:02 +0300

    in

---
+++ src/com/lincomengineering/lincomroip/MainActivity.java
@@ -0,0 +1,257 @@
+package com.lincomengineering.lincomroip;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.ServiceConnection;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lincomengineering.lincomroip.LCRService.LocalBinder;
+
+public class MainActivity extends Activity {
+	public native void setEnv();
+	private ImageButton btnPtt;
+	private SeekBar seekSql;
+	private TextView txtContact, txtChan;
+	private Button btnConnect, btnWatt;
+	private int current_call_id = -1;
+	LCRService mService;
+    boolean mBound = false;
+    private Context context;
+    static Handler h;
+    private int connected = 0;
+
+    static {
+        System.loadLibrary("lincomroip");
+    }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+        setContentView(R.layout.activity_main);
+        txtContact = (TextView) findViewById(R.id.txtContact);
+        txtContact.setText("192.168.6.30");
+        txtChan = (TextView) findViewById(R.id.textChan);
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnWatt = (Button) findViewById(R.id.btnWatt);
+        btnWatt.setText("--W");
+        btnPtt = (ImageButton) findViewById(R.id.btnPtt);
+        h = new Handler() {
+	         public void handleMessage(Message msg) {
+	        	 int chan;
+	             Bundle b = msg.getData();
+	             String status = b.getString("callback_string");
+	             //Toast.makeText(context, status.substring(0,8), Toast.LENGTH_SHORT).show();
+	             if (status.length() > 14) {
+		             if (((String)status.substring(0,8)).equals("Channel:")) {
+			             chan = Integer.parseInt(status.substring(8, 11))-233;
+			             txtChan.setText(String.format("%02d", chan));
+			             btnWatt.setText(String.format("%sW", status.substring(12, 14)));
+		        	 } else Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+	             } else Toast.makeText(context, status, Toast.LENGTH_SHORT).show();
+	         }
+	     };
+        btnPtt.setOnTouchListener(new OnTouchListener() {
+        	
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+    	        switch ( event.getAction() ) {
+    		        case MotionEvent.ACTION_DOWN: 
+    		        	if (current_call_id != -1) {
+    		        		//lincomroip.sendmsgLincomRoIP(current_call_id, "J1");
+    		        		SendLinCmd("J1");
+    		        	} else {
+    		        		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    		        	}
+    		        	break;
+    		        case MotionEvent.ACTION_UP: 
+    		        	if (current_call_id != -1) {
+    		        		//lincomroip.sendmsgLincomRoIP(current_call_id, "J0");
+    		        		SendLinCmd("J0");
+    		        	} else {
+    		        		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    		        	}
+    		        	break;
+    	        }
+    	        return false;
+    	    }
+        });
+        seekSql = (SeekBar) findViewById(R.id.seekSql);
+        seekSql.setProgress(30);
+        seekSql.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				int progress = ((SeekBar)v).getProgress();
+    	        switch ( event.getAction() ) {
+    		        case MotionEvent.ACTION_UP: 
+    		        	if (current_call_id != -1) {
+    		        		String.format("S%X", (progress*15)/100);
+    		        		//lincomroip.sendmsgLincomRoIP(current_call_id, String.format("S%X", (progress*15)/100));
+    		        		SendLinCmd(String.format("S%X", (progress*15)/100));
+    		        	} else {
+    		        		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    		        	}
+    		        	break;
+    	        }
+    	        return false;
+    	    }
+        });
+        /* Load native library */
+//        try {
+//        	//System.loadLibrary("stlport_shared");
+//        	System.loadLibrary("lincomroip");
+//        	Log.d("LincomRoIP","Library load OK\n");
+//        	Toast.makeText(getApplicationContext(), "Library load OK", Toast.LENGTH_SHORT).show();
+//        } catch (UnsatisfiedLinkError e) {
+//        	Log.d("LincomRoIP",e.getMessage());
+//        	this.finish();
+//        	return;
+//        }
+        //setEnv();
+        //lincomroip.initLincomRoIP();
+
+        //lincomroip.setToastCallback((SWIGTYPE_p_void)toastCallback);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+//        Intent intent = new Intent(this, LCRService.class);
+//        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+        //setEnv();
+        lincomroip.initLincomRoIP();
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+//        if (mBound) {
+//            unbindService(mConnection);
+//            mBound = false;
+//        }
+        lincomroip.destroyLincomRoIP();
+    }
+    
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //lincomroip.destroyLincomRoIP();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    public void doConnect(View v) {
+    	if (connected == 0) {
+	    	String contact = String.format("sip:%s", txtContact.getText().toString());
+	    	current_call_id = lincomroip.connectLincomRoIP(contact);
+	    	btnConnect.setText("Disconnect");
+	    	connected = 1;
+    	} else {
+    		lincomroip.disconnectLincomRoIP();
+    		btnConnect.setText("Connect");
+    		btnWatt.setText("--W");
+    		txtChan.setText("--");
+    		connected = 0;
+    	}
+	}
+    
+    public void chanUp(View v) {
+    	if (current_call_id != -1) {
+    		//lincomroip.sendmsgLincomRoIP(current_call_id, "1");
+    		SendLinCmd("1");
+    	} else {
+    		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    	}
+	}
+    
+    public void chanDown(View v) {
+    	if (current_call_id != -1) {
+    		//lincomroip.sendmsgLincomRoIP(current_call_id, "5");
+    		SendLinCmd("5");
+    	} else {
+    		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    	}
+	}
+    
+    public void changeWatt(View v) {
+    	if (current_call_id != -1) {
+    		//lincomroip.sendmsgLincomRoIP(current_call_id, "W");
+    	} else {
+    		Toast.makeText(getApplicationContext(), "No active connection!", Toast.LENGTH_SHORT).show();
+    	}
+	}
+    
+    public void sendMsg(View v) {
+    	lincomroip.sendmsgLincomRoIP(current_call_id, "alabala");
+	}
+    
+    public void toastCallback(String s) {
+    	Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+    
+    public void pagerCallback(String s) {
+    	//Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    	Log.d("LincomRoIP","pagerCallback\n");
+    }
+    
+    public void SendLinCmd(String s) {
+    	btnWatt.setText("--W");
+		txtChan.setText("--");
+    	lincomroip.sendmsgLincomRoIP(current_call_id, s);
+    }
+    
+    public void callBack(String s) {
+    	Log.d("LincomRoIP",s);
+    	try {
+    		Bundle b = new Bundle();
+            b.putString("callback_string", s);
+            Message m = Message.obtain();
+            m.setData(b);
+            m.setTarget(h);
+            m.sendToTarget();
+    	} catch (Exception e) {
+    		Log.e("LincomRoIP","eeeerrrorrr");
+    	}
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalBinder binder = (LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            //mService.retrieveClockHS();
+            //ClockHSActivity.this.loadWeb();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+}
---
+++ swig/lincomroip.cxx
@@ -0,0 +1,268 @@
+#include <pjsua-lib/pjsua.h>
+#include <android/log.h>
+#include <jni.h>
+
+#define THIS_FILE	"LincomRoIP"
+
+static void callback_handler(char *s);
+int sendmsgLincomRoIP(int call_id, char *s);
+
+/* Callback called by the library upon receiving incoming call */
+static void on_incoming_call(pjsua_acc_id acc_id, pjsua_call_id call_id,
+			     pjsip_rx_data *rdata)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(acc_id);
+    PJ_UNUSED_ARG(rdata);
+
+    pjsua_call_get_info(call_id, &ci);
+
+    PJ_LOG(3,(THIS_FILE, "Incoming call from %.*s!!",
+			 (int)ci.remote_info.slen,
+			 ci.remote_info.ptr));
+
+    /* Automatically answer incoming calls with 200/OK */
+    pjsua_call_answer(call_id, 200, NULL, NULL);
+}
+
+/* Callback called by the library when call's state has changed */
+static void on_call_state(pjsua_call_id call_id, pjsip_event *e)
+{
+    pjsua_call_info ci;
+
+    PJ_UNUSED_ARG(e);
+
+    pjsua_call_get_info(call_id, &ci);
+    PJ_LOG(3,(THIS_FILE, "Call %d state=%.*s", call_id,
+			 (int)ci.state_text.slen,
+			 ci.state_text.ptr));
+		if (ci.state == PJSIP_INV_STATE_CONFIRMED) sendmsgLincomRoIP(call_id,"?");
+}
+
+/* Callback called by the library when call's media state has changed */
+static void on_call_media_state(pjsua_call_id call_id)
+{
+    pjsua_call_info ci;
+
+    pjsua_call_get_info(call_id, &ci);
+
+    if (ci.media_status == PJSUA_CALL_MEDIA_ACTIVE) {
+	// When media is active, connect call to sound device.
+	pjsua_conf_connect(ci.conf_slot, 0);
+	pjsua_conf_connect(0, ci.conf_slot);
+			//sendmsgLincomRoIP(call_id,"?");
+    }
+}
+
+static void on_pager(pjsua_call_id call_id, const pj_str_t *from, 
+                     const pj_str_t *to, const pj_str_t *contact,
+                     const pj_str_t *mime_type, const pj_str_t *text)
+{
+    /* Note: call index may be -1 */
+    PJ_UNUSED_ARG(to);
+    PJ_UNUSED_ARG(contact);
+    PJ_UNUSED_ARG(mime_type);
+
+    PJ_LOG(3,(THIS_FILE,"MESSAGE from %.*s: %.*s (%.*s)",
+              (int)from->slen, from->ptr,
+              (int)text->slen, text->ptr,
+              (int)mime_type->slen, mime_type->ptr)); 
+
+   if (strncmp (text->ptr,"ICOM:",5) == 0) callback_handler(text->ptr+5); //pager_cb(text->ptr+5);
+}
+
+void printlog(const char *format, ...)
+{
+    va_list arg;
+    
+    va_start(arg, format);
+
+    __android_log_vprint(ANDROID_LOG_INFO, THIS_FILE, format, arg);
+
+    va_end(arg);
+}
+
+void showLog(int level, const char *data, int len)
+{
+		//toast_cb((char *)data);
+	//callback_handler((char *)data);
+    printlog("%s", data);
+}
+
+int initLincomRoIP() {
+   pjsua_acc_id acc_id;
+   pj_status_t status;
+   pjsua_transport_id transport_id = -1;
+   char errbuf[256];
+
+    /* Create pjsua first! */
+   status = pjsua_create();
+   if (status != PJ_SUCCESS) {
+      printlog("pjsua_create failed ");    
+      return -1;
+   }
+
+   /* Init pjsua */
+   {
+      pjsua_config cfg;
+      pjsua_logging_config log_cfg;
+			pjsua_media_config media_cfg;
+
+      pjsua_config_default(&cfg);
+      cfg.cb.on_incoming_call = &on_incoming_call;
+      cfg.cb.on_call_media_state = &on_call_media_state;
+      cfg.cb.on_call_state = &on_call_state;
+      cfg.cb.on_pager = &on_pager;
+
+      pjsua_logging_config_default(&log_cfg);
+      log_cfg.console_level = 4;
+      log_cfg.cb = &showLog;
+
+      //cfg.stun_host = pj_str("stun.ekiga.net");
+
+			pjsua_media_config_default(&media_cfg);
+			media_cfg.ec_tail_len = 0;
+
+      status = pjsua_init(&cfg, &log_cfg, &media_cfg);
+      if (status != PJ_SUCCESS) {
+         printlog("pjsua_init failed ");    
+         return -1;
+      }
+   }
+
+   /* Add UDP transport. */
+   {
+      pjsua_transport_config cfg;
+
+      pjsua_transport_config_default(&cfg);
+      cfg.port = 5060;
+      status = pjsua_transport_create(PJSIP_TRANSPORT_UDP, &cfg, &transport_id);
+      if (status != PJ_SUCCESS) {
+         printlog("pjsua_transport_create failed: %s", pj_strerror(status, errbuf, sizeof(errbuf)));    
+         return -1;  
+      }
+   }
+
+   /* Initialization is done, now start pjsua */
+   status = pjsua_start();
+   if (status != PJ_SUCCESS)  {
+      printlog("pjsua_start failed ");    
+      return -1;  
+   }
+
+    /* Create local SIP account. */
+   {
+      status = pjsua_acc_add_local(transport_id, PJ_TRUE, &acc_id);
+      if (status != PJ_SUCCESS)  {
+         printlog("pjsua_acc_add_local");    
+         return -1;  
+      }
+			pjsua_acc_set_online_status(acc_id, PJ_TRUE);
+   }
+		//callback_handler((char *)"LincomRoIP initialized!");
+   return 0;
+}
+
+int connectLincomRoIP(char *s) {
+   pj_status_t status;
+   pjsua_call_id call_id = -1;
+   pj_str_t uri = pj_str(s);
+   pjsua_call_make_call(pjsua_acc_get_default(), &uri, 0, NULL, NULL, &call_id);
+   return call_id;
+}
+
+int disconnectLincomRoIP() {
+	pjsua_call_hangup_all();
+}
+
+int sendmsgLincomRoIP(int call_id, char *s) {
+	 char amsg[80];
+	 pj_ansi_sprintf(amsg, "VCMD:%s",s);
+   pj_str_t msg = pj_str(amsg);
+   return pjsua_call_send_im( call_id, NULL, &msg, NULL, NULL);
+}
+
+int destroyLincomRoIP() {
+   pjsua_destroy();
+}
+
+static JavaVM *gJavaVM;
+static jobject gInterfaceObject, gDataObject;
+const char *kInterfacePath = "com/lincomengineering/lincomroip/MainActivity";
+static jclass interfaceClass;
+
+void initClassHelper(JNIEnv *env, const char *path, jobject *objptr) {
+	jclass cls = env->FindClass(path);
+	if(!cls) {
+	printlog("initClassHelper: failed to get %s class reference", path);
+	return;
+	}
+	jmethodID constr = env->GetMethodID(cls, "<init>", "()V");
+	if(!constr) {
+	printlog("initClassHelper: failed to get %s constructor", path);
+	return;
+	}
+	jobject obj = env->NewObject(cls, constr);
+	if(!obj) {
+	printlog("initClassHelper: failed to create a %s object", path);
+	return;
+	}
+	(*objptr) = env->NewGlobalRef(obj);
+}
+
+extern "C" jint JNI_OnLoad(JavaVM* vm, void* reserved)
+{
+	JNIEnv *env;
+	gJavaVM = vm;
+	printlog("JNI_OnLoad called");
+	if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK) {
+		printlog("Failed to get the environment using GetEnv()");
+		return -1;
+	}
+	interfaceClass = env->FindClass(kInterfacePath);
+	if(!interfaceClass) {
+		printlog("callback_handler: failed to get class reference");
+		return -1;
+	}
+	return JNI_VERSION_1_4;
+}
+
+static void callback_handler(char *s) {
+	int status;
+	JNIEnv *env;
+	bool isAttached = false;
+	status = gJavaVM->GetEnv((void **) &env, JNI_VERSION_1_4);
+	if(status < 0) {
+	printlog("callback_handler: failed to get JNI environment, "
+	"assuming native thread");
+	status = gJavaVM->AttachCurrentThread(&env, NULL);
+	if(status < 0) {
+	printlog("callback_handler: failed to attach "
+	"current thread");
+	return;
+	}
+	isAttached = true;
+	}
+	/* Construct a Java string */
+	jstring js = env->NewStringUTF(s);
+	//initClassHelper(env, kInterfacePath, &gInterfaceObject);
+	//jclass interfaceClass = env->GetObjectClass(gInterfaceObject);
+	//jclass interfaceClass = env->FindClass(kInterfacePath);
+	//if(!interfaceClass) {
+	//printlog("callback_handler: failed to get class reference");
+	//if(isAttached) gJavaVM->DetachCurrentThread();
+	//return;
+	//}
+	/* Find the callBack method ID */
+	//jmethodID method = env->GetStaticMethodID(
+	jmethodID method = env->GetMethodID(
+	interfaceClass, "callBack", "(Ljava/lang/String;)V");
+	if(!method) {
+	printlog("callback_handler: failed to get method ID");
+	if(isAttached) gJavaVM->DetachCurrentThread();
+	return;
+	}
+	env->CallStaticVoidMethod(interfaceClass, method, js);
+	if(isAttached) gJavaVM->DetachCurrentThread();
+}
